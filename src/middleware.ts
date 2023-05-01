7f5d8a4448253dc519454dd4c63c1b0163760b91
jettblu <jettblu.fabrications@gmail.com>
2023-05-01 11:43:31 -0400

    Add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,47 @@
+import {} from "cookies-next";
+import { NextApiResponse } from "next";
+import { jwtVerify } from "jose";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest, res: NextApiResponse) {
+  const cookies = req.cookies;
+  const accessToken = cookies.get("accessToken")?.value;
+  const refreshToken = cookies.get("refreshToken")?.value;
+  if (!accessToken || !refreshToken || !process.env.JWT_ACCESS_SECRET) {
+    // req.nextUrl.searchParams.set("from", req.nextUrl.pathname);
+    req.nextUrl.pathname = "/demo";
+    return NextResponse.redirect(req.nextUrl);
+  }
+  const secret = process.env.JWT_ACCESS_SECRET;
+  const adaptedSecret: Uint8Array = new TextEncoder().encode(secret);
+  try {
+    const { payload, protectedHeader } = await jwtVerify(
+      accessToken,
+      adaptedSecret
+    );
+    const requestHeaders = new Headers(req.headers);
+    // Add new request headers
+    requestHeaders.set("user-id", `${payload.userId}`);
+
+    // You can also set request headers in NextResponse.rewrite
+    const response = NextResponse.next({
+      request: {
+        // New request headers
+        headers: requestHeaders,
+      },
+    });
+    return response;
+  } catch (e: any) {
+    // req.nextUrl.searchParams.set("from", req.nextUrl.pathname);
+    req.nextUrl.pathname = "/demo";
+    return NextResponse.redirect(req.nextUrl);
+  }
+}
+
+export const config = {
+  matcher: [
+    "/api/user/:path*", // matches /api/user/1, /api/user/2, etc.
+    "/profile/:path*", // matches /profile/1, /profile/2, etc.,
+    "/profile", // matches /profile
+  ],
+};
