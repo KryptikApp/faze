b5ddabc707dea75e1fd3da09856b46d094b66e74
jettblu <jettblu.fabrications@gmail.com>
2023-05-01 18:40:04 -0400

    Update test api

--- src/pages/api/auth/test.ts
+++ src/pages/api/auth/test.ts
@@ -1,6 +1,7 @@
 // this file is responsible for registering face encodings for a user
 
 import { NextApiRequest, NextApiResponse } from "next";
+import { getUserByUsername } from "../../../../prisma/script";
 
 type Data = {
   msg?: string;
@@ -11,7 +12,8 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  const user = await getUserByUsername("jett");
   return res.status(200).json({
-    msg: "Test successful.",
+    msg: "Test successful." + user?.username,
   });
 }
